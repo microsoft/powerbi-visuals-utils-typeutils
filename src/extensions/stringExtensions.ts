0808e65dd0b864bcc4a4cc9b13213c40655a3d18
Alexey <39551830+Demonkratiy@users.noreply.github.com>
2022-08-31 16:44:28 +0200

    moved to ESlint, update utils (#55)

--- src/extensions/stringExtensions.ts
+++ src/extensions/stringExtensions.ts
@@ -26,11 +26,9 @@
 /**
  * Extensions to String class.
  */
-export module StringExtensions {
-    /**
-     * Checks if a string ends with a sub-string.
-     */
-    export function endsWith(str: string, suffix: string): boolean {
-        return str.indexOf(suffix, str.length - suffix.length) !== -1;
-    }
+/**
+ * Checks if a string ends with a sub-string.
+ */
+export function endsWith(str: string, suffix: string): boolean {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
